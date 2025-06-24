2eb65ba5b9acc7e2184f74cbfe80e79a34a96f17
Jann Erhardt <CraftZockerLP@gmail.com>
2025-06-24 15:15:34 +0200

    feat(go): Small go improvement

--- state/distribution.go
+++ state/distribution.go
@@ -3,14 +3,14 @@ package state
 import "sync"
 
 const (
-	RuntimeModeHeadless int8 = 0 // Headless mode, no GUI, distribution modes available: Standalone, Control, Voice
-	RuntimeModeGUI      int8 = 1 // GUI mode, using Wails for GUI & Standalone server, only supports Standalone distribution mode
+	RuntimeModeHeadless int8 = iota // Headless mode, no GUI, distribution modes available: Standalone, Control, Voice
+	RuntimeModeGUI                  // GUI mode, using Wails for GUI & Standalone server, only supports Standalone distribution mode
 )
 
 const (
-	DistributionModeStandalone int8 = 0 // Standalone mode, no distribution all in one Server
-	DistributionModeControl    int8 = 1 // Only Control Server, no Voice. Used as Control-Node for Voice Servers
-	DistributionModeVoice      int8 = 2 // Only Voice Server, no Control. Used as Voice-Node for Control Servers
+	DistributionModeStandalone int8 = iota // Standalone mode, no distribution all in one Server
+	DistributionModeControl                // Only Control Server, no Voice. Used as Control-Node for Voice Servers
+	DistributionModeVoice                  // Only Voice Server, no Control. Used as Voice-Node for Control Servers
 )
 
 type DistributionState struct {
