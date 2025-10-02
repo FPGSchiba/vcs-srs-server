591f4f36ae022a84d6d7a49988e04bd3e1413ca0
Jann Erhardt <CraftZockerLP@gmail.com>
2025-10-02 07:55:03 +0200

    fix: FIx test runs

--- app/app_test.go
+++ app/app_test.go
@@ -4,14 +4,15 @@ import (
 	"testing"
 
 	"github.com/FPGSchiba/vcs-srs-server/state"
+	"github.com/google/uuid"
 )
 
 func newTestApp() *VCSApplication {
 	settings := &state.SettingsState{}
 	admin := &state.AdminState{}
 	server := &state.ServerState{
-		Clients:      make(map[string]*state.ClientState),
-		RadioClients: make(map[string]*state.RadioState),
+		Clients:      make(map[uuid.UUID]*state.ClientState),
+		RadioClients: make(map[uuid.UUID]*state.RadioState),
 		BannedState:  state.BannedState{BannedClients: []state.BannedClient{}},
 	}
 	return &VCSApplication{
